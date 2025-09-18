3125eec27d261a4e93564dc671db5cde8c09b009
Gluttony <sivan.g1307@gmail.com>
2025-09-18 16:46:05 -0400

    Fix CSS path for GitHub Pages

---
+++ frontend/docs/_next/static/chunks/3a18d522cfb6f38c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/ee8805d0a44ac57d.js",
+  "static/chunks/b4a712c0b8f6077e.js",
+  "static/chunks/turbopack-72609d7e502e19af.js"
+])
---
+++ frontend/docs/_next/static/chunks/acc9652960871608.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/a6a8b147318253ee.js",
+  "static/chunks/b4a712c0b8f6077e.js",
+  "static/chunks/turbopack-4c4a478f181dd81d.js"
+])
