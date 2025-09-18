9d6a08e8b667c7f6358defb04575416a4b9ed2c6
Gluttony <sivan.g1307@gmail.com>
2025-09-18 11:29:55 -0400

    Deploy static site to GitHub Pages

--- frontend/docs/_next/static/chunks/c4f4b11dea59644d.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/4cc232b64a7e7155.js",
-  "static/chunks/turbopack-ce3ac7d624836328.js"
-])
--- frontend/docs/_next/static/chunks/ed944cff9f037099.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/624e00bbce716cbd.js",
-  "static/chunks/turbopack-feb6c24d20c1e12a.js"
-])
